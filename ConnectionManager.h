5ec0b67c768f9178230182f5794214bdc4703938
22546723 <81229137+22546723@users.noreply.github.com>
2024-03-08 13:57:51 +0200

    firebase, wifi and bluetooth working

--- ConnectionManager.h
+++ ConnectionManager.h
@@ -1,23 +1,63 @@
 #include <cstdint>
 #include <Arduino.h>
 #include <Preferences.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID_SCAN             "e1f6fa49-170d-4629-bd77-ea170a0309dc"
+#define CHARACTERISTIC_UUID_SCAN      "27ce423b-bcc6-4fe7-aa98-0d502e79f15a"
+#define CHARACTERISTIC_UUID_SCAN_NAME "9a61b953-5e81-45f7-b998-4c54d213e710"
+#define CHARACTERISTIC_UUID_SCAN_NEW  "ffeca8e2-4285-4949-9819-66364ef72d25"
+
+#define SERVICE_UUID_WIFI_SELECT           "c3313912-4f44-4c5d-abe4-6a99dbe5274f"
+#define CHARACTERISTIC_UUID_WIFI_NAME      "62a140cc-0bcf-49da-94e3-fba705938272"
+#define CHARACTERISTIC_UUID_WIFI_PASSWORD  "1603d3a3-7500-4e90-8310-6e7b12e7bfa4"
+#define CHARACTERISTIC_UUID_WIFI_SET       "1675752a-2a8e-48e8-99ef-ee90ff878d0f" 
+#define CHARACTERISTIC_UUID_WIFI_CONNECTED "792920e9-f1ff-4587-879a-8cefb09c18d2"
+
+#define SERVICE_UUID_CONNECT          "a676b1bc-61f9-4af6-8dae-28831bad2ec6"
+#define CHARACTERISTIC_UUID_DEVICE    "9fbf707f-c39a-418a-828c-6e291af51ac4"
 
 #ifndef CONNECTION_MANAGER_H
 #define CONNECTION_MANAGER_H
+
+
 
 class ConnectionManager
 {
     public:
         explicit ConnectionManager();
-        bool getConnectionStatus();
+
+        void setupBluetooth(String board_name);
+        void sendWifiNetworks();
         uint8_t connectToWifi();
-        void setWifi(char* wifi_ssid, char* wifi_password);
+        bool setupWifi();
         bool haveWifiConfig();
+        bool getConnectionStatus();
+        void setName(String name);
+
 
     protected:
 
     private:
       Preferences con_preferences;
+      BLEServer *pServer;
+      
+      BLEService *pServiceScan;
+      BLECharacteristic *pCharacteristicScan;
+      BLECharacteristic *pCharacteristicScanName;
+      BLECharacteristic *pCharacteristicScanNew;
+
+      BLEService *pServiceSelect;
+      BLECharacteristic *pCharacteristicWifiSet;
+      BLECharacteristic *pCharacteristicWifiName;
+      BLECharacteristic *pCharacteristicWifiPassword;
+      BLECharacteristic *pCharacteristicWifiConnected;
+
+      BLEService *pServiceConnect;
+      BLECharacteristic *pCharacteristicDeviceName;
 };
+
 
 #endif // CONNECTION_MANAGER_H
--- DataLogger.cpp
+++ DataLogger.cpp
@@ -9,25 +9,47 @@ DataLogger::DataLogger( uint8_t max_log )
     log_count = 0;
   } // end constructor
 
+
+/**
+ * Set the amount of readings to average and uload to firestore 
+ *
+ * @param max_log number of readings
+ */
 void DataLogger::setMaxLogs(uint8_t max_log) {
   max_log_count = max_log; 
 }
 
+
+/**
+ * Return the amount of readings to average and upload to firestore 
+ *
+ * @return number of readings
+ */
 uint8_t DataLogger::getMaxLogs() {
   return max_log_count;
 }
 
+
+/**
+ * Add readings to totals and return the running average
+ *
+ * @return tuple <has the max logs been reached | data 1 avg | data 2 avg>
+ */
 std::tuple<bool, uint8_t, uint8_t> DataLogger::logData(uint16_t data1, uint16_t data2) {
 
+  // Update totals
   data_total[0] = data_total[0] + data1;
   data_total[1] = data_total[1] + data2;
 
-  uint8_t data_out[2] = {0, 0};
+  // Calculate running avg
+  uint8_t data_out[2];
+  data_out[0] = data_total[0]/log_count;
+  data_out[1] = data_total[1]/log_count;
+
   bool max_logs_reached = false;
 
+  // Check if max logs has been reached
   if (log_count >= max_log_count) {
-    data_out[0] = data_total[0]/log_count;
-    data_out[1] = data_total[1]/log_count;
     log_count = 0;
     data_total[0] = 0;
     data_total[1] = 0;
